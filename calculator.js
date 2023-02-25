de809f566efe56004792bc2415577081066ed080
Siv Earley <searley96@gmail.com>
2023-02-25 17:48:03 -0600

    delete button removes employee from dom

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-let employees = [];
+let allEmployees = [];
 
 $(document).ready(readyNow);
 
@@ -7,15 +7,43 @@ function readyNow() {
 
     //listener for submit button
     $("#submitEmployeeBtn").on("click", addNewEmployee);
+
+    // listener for delete button
+    $("#employeeList").on("click", ".deleteBtn", deleteEmployee);
+}
+
+function deleteEmployee(){
+    console.log("Inside of deleteEmployee()");
+
+    // let deleteBtn = $(this);
+    // new array for deleted employees
+    let newEmployees = [];
+    //use ID number to delete from employees[]
+    const idNumberToDelete = $(this).parent().siblings().first().text();
+    //console should show the ID number for employee to delete
+    console.log("Employee to delete:", idNumberToDelete);
+
+    for (let employee of allEmployees) {
+       
+        if (employee.firstName !== idNumberToDelete) {
+            newEmployees.push(employee);
+        }
+       
+    }
+    allEmployees = newEmployees;
+    console.log("these are new employees", newEmployees);
+    console.log("these are all employees", allEmployees);
+    render();
+
 }
 
 function addNewEmployee() {
     console.log("Inside of addNewEmployee()");
-    console.log("Employees before new employee added:", employees);
+    console.log("Employees before new employee added:", allEmployees);
 
     //get input values
     const firstNameInputValue = $("#firstNameInput").val();
-    const lastNameInputValue =  $("#lastNameInput").val();
+    const lastNameInputValue = $("#lastNameInput").val();
     const idNumberInputValue = $("#idNumber").val();
     const titleInputValue = $("#jobTitle").val();
     const annualSalaryInputValue = $("#annualSalary").val();
@@ -32,15 +60,44 @@ function addNewEmployee() {
     //capture employee values outside an object
 
     const newEmployeeToAdd = {
-        firstName: firstNameInputValue, 
-        lastName: idNumberInputValue,
+        firstName: firstNameInputValue,
+        lastName: lastNameInputValue,
         idNumber: idNumberInputValue,
         jobTitle: titleInputValue,
-        annualSalary: nnualSalaryInputValue
+        annualSalary: annualSalaryInputValue
     };
 
     //add to array
-    employees.push(newEmployeeToAdd);
-    console.log("Employees after new employee added:", employees);
+    allEmployees.push(newEmployeeToAdd);
+    console.log("Employees after new employee added:", allEmployees);
 
+    //render the DOM after new employee added
+    //re-render when new employee added
+    render();
+
+}
+
+function render() {
+    //reset employee list
+    $("#employeeList").empty();
+
+    //add employees and delete button to DOM
+    for (let employee of allEmployees) {
+
+
+        $("#employeeList").append(`
+        <tr>
+            <td>${employee.firstName}</td>
+            <td>${employee.lastName}</td>
+            <td>${employee.idNumber}</td>
+            <td>${employee.jobTitle}</td>
+            <td>${employee.annualSalary}</td>
+            <td>
+                <button class="deleteBtn">
+                Delete
+                </button>
+            </td>
+        </tr>
+        `);
+    };
 }
